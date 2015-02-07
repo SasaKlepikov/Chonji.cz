4406aad88bf65bcb9657033031695a9ca5518f08
Alexandr Klepikov <alexandr.klepikov@aira.cz>
2015-02-07 03:29:59 +0100

    Modif: Taxonomy technik + CSS. New modules: Flexslider & marup

--- sites/all/themes/bartik/js/custom.js
+++ sites/all/themes/bartik/js/custom.js
@@ -1,6 +1,15 @@
 (function ($) {
     $(document).ready(function () {
 
+        $('.flexslider .flex-viewport .slides li img').each(function () {
+            $(this).addClass('tech-gal');
+            $(this).attr('href',$(this).attr('src'));
+            /*$(this).click(function () {
+                $(this).colorbox({href: $(this).attr('src'), rel: 'tech-gal'});
+            });*/
+        });
+        $('.flexslider .flex-viewport .slides li img').colorbox({rel: 'tech-gal', current: false});
+
         // Hiding AHAH PROCESSED button
         $('.field-name-field-fotografie-z-akce .field-items .field-item').hide();
         // Inserting button "add more rows"
@@ -23,7 +32,7 @@
         // Show min count of rows
         $('.field-name-field-fotografie-z-akce .field-items .field-item:lt(' + show_rows + ')').show();
         //
-        
+
         // checking, if all images are showen at beganing
         if (show_rows >= total_rows_count) {
             $('.more-images-button .more-images-button-label').hide();
@@ -36,23 +45,23 @@
                 $('.more-images-button .more-images-button-label').hide();
             }
         });
-        
+
         $('#block-user-login #user-login-form').hide();
         $('<div class="user-login-button"><div id="user-login-button">Přihlásit se / Registrovat</div></div>').insertBefore('#block-user-login #user-login-form');
         $('<div class="user-login-button-hide" style="display:none;"><div id="user-login-button-hide">Skrýt</div></div>').insertAfter('#block-user-login #user-login-form');
-        $('#block-user-login #user-login-button').click(function(){
+        $('#block-user-login #user-login-button').click(function () {
             $('#block-user-login .user-login-button').hide();
             $('#block-user-login .user-login-button-hide').show(200);
             $('#block-user-login #user-login-form').show(200);
         });
-        $('#block-user-login #user-login-button-hide').click(function(){
+        $('#block-user-login #user-login-button-hide').click(function () {
             $('#block-user-login .user-login-button-hide').hide();
             $('#block-user-login .user-login-button').show(200);
             $('#block-user-login #user-login-form').hide(200);
         });
-        
-        if($('body.node-type-zak-skoly')){
-            $('body.node-type-zak-skoly .field-name-field-zak-zkousky table tbody .field_zkouska_technicky_stupen').each(function(){
+
+        if ($('body.node-type-zak-skoly')) {
+            $('body.node-type-zak-skoly .field-name-field-zak-zkousky table tbody .field_zkouska_technicky_stupen').each(function () {
                 var NewClass = $(this).find('ul.links li').text();
                 var NewClassNumbers = NewClass.replace(/[^0-9]/g, "");
                 var NewClassLetters = NewClass.replace(/[^a-zA-Z]/g, "").toLowerCase();
@@ -60,8 +69,8 @@
                 $(this).addClass(NewClassKonstruktor);
             });
         }
-        if($('body.page-seznam-nasich-zaku')){
-            $('body.page-seznam-nasich-zaku .view-seznam-nasich-zaku table tbody tr .views-field-field-zak-zkousky .field-name-field-zkouska-technicky-stupen').each(function(){
+        if ($('body.page-seznam-nasich-zaku')) {
+            $('body.page-seznam-nasich-zaku .view-seznam-nasich-zaku table tbody tr .views-field-field-zak-zkousky .field-name-field-zkouska-technicky-stupen').each(function () {
                 var NewClass = $(this).find('ul.links li').text();
                 var NewClassNumbers = NewClass.replace(/[^0-9]/g, "");
                 var NewClassLetters = NewClass.replace(/[^a-zA-Z]/g, "").toLowerCase();
